7e3cc109b520b8b6db9ad4d83d33e3190a97c6d6
mmzy12 <126200730+mmzy12@users.noreply.github.com>
2024-03-29 00:41:00 +0800

    Update subconf.sh

--- clashconf/subconf.sh
+++ clashconf/subconf.sh
@@ -24,4 +24,15 @@ sed -i '0,/^;设置规则标志位/ { /^;设置规则标志位/ r '"$ruleset_fil
 # 插入内容
 sed -i "/custom_proxy_group=♻️ 自动选择\`url-test\`.\*\`http:\/\/www.gstatic.com\/generate_204\`300,,50/r $proxy_group_file" clashconf.ini
 
+# 定义要修改的文件路径
+file_path="clashconf.ini"
+
+# 定义原始文本和替换文本
+original_text="♻️ 自动选择\`url-test\`.*\`http"
+replacement_text="♻️ 自动选择\`url-test\`(^(?!.*(回家|home|back)).*)\`http"
+
+# 使用sed命令在文件中进行替换
+sed -i "s/${original_text}/${replacement_text}/g" "$file_path"
+
+
 echo "配置已更新"
